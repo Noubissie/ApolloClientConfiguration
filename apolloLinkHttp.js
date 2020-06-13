99a93e9d1746e7c8302691d62093b91983e2f2e0
Noubissie <55121186+Noubissie@users.noreply.github.com>
2020-06-13 16:04:39 +0100

    Update apolloLinkHttp.js

--- apolloLinkHttp.js
+++ apolloLinkHttp.js
@@ -6,7 +6,6 @@ let HTTPlink = (Token)=>{
         new HttpLink(
             {
                 uri: "http://localhost:5000/g",
-                // uri:"http://192.168.43.137:5000/g",
                 credentials:"include", //same-origin
                 header:{
                     authetication:Token,
